2f0f3e1c6f1ca46727a213ef542bf96135533c33
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2022-08-28 13:06:34 +0900

    map 메소드를 사용하여 반복되는 html 소스 생성

--- src/App.js
+++ src/App.js
@@ -19,17 +19,34 @@ export default class App extends Component {
     }
   }
 
+  todoData = [
+    {
+      id: "1",
+      title : "공부하기",
+      complated: true
+    },
+    {
+      id: "2",
+      title : "청소하기",
+      complated: false
+    }
+  ]
+
   render() {
     return(
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
-            <div style={this.getStyle()}>
+
+            {this.todoData.map((data) => (
+              <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={false} />
-              공부하기
+              {data.title}
               <button style={this.btnStyle}>x</button>
             </div>
+            ))}
+            
           </div>
         </div>
       </div>
