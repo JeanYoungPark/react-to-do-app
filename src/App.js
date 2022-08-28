ee6142e8a8e9aeac580b5655476329ac5c5baaf0
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2022-08-28 13:31:01 +0900

    filter & state를 이용해 돔 리랜더링 하기

--- src/App.js
+++ src/App.js
@@ -2,36 +2,45 @@ import React, {Component} from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  state = {
+    todoData : [
+      {
+        id: "1",
+        title : "공부하기",
+        complated: true
+      },
+      {
+        id: "2",
+        title : "청소하기",
+        complated: false
+      }
+    ],
+    value: "",
+  }
+
   btnStyle = {
-    color:"#fff",
-    border:"none",
-    padding:"5px 9px",
-    borderRadius:"50%",
-    cursor:"pointer",
-    float:"right"
+    color : "#fff",
+    border : "none",
+    padding : "5px 9px",
+    borderRadius : "50%",
+    cursor : "pointer",
+    float : "right"
   }
 
   getStyle = () => {
     return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: "none"
+      padding : "10px",
+      borderBottom : "1px #ccc dotted",
+      textDecoration : "none"
     }
   }
 
-  todoData = [
-    {
-      id: "1",
-      title : "공부하기",
-      complated: true
-    },
-    {
-      id: "2",
-      title : "청소하기",
-      complated: false
-    }
-  ]
-
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id)
+    this.setState({todoData : newTodoData})
+  }
+  
   render() {
     return(
       <div className="container">
@@ -39,11 +48,11 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
 
-            {this.todoData.map((data) => (
+            {this.state.todoData.map((data) => (
               <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={false} />
               {data.title}
-              <button style={this.btnStyle}>x</button>
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
             </div>
             ))}
             
