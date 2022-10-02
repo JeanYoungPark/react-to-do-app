d637814f9a526b4e596313c1e9b955672767fd5a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2022-10-02 14:55:16 +0900

    tailwindcss setting restyling css

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,6 @@
 import React from "react";
 
 export default function List({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const listStyle = (complated) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: complated ? "line-through" : "none",
-    };
-  };
-
   const handleClick = (id) => {
     let newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -36,16 +19,27 @@ export default function List({ todoData, setTodoData }) {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={listStyle(data.complated)} key={data.id}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={() => handleComplateChange(data.id)}
-          />
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>
-            x
-          </button>
+        <div key={data.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => handleComplateChange(data.id)}
+              />
+              <span className={data.complated ? "line-through pl-2" : "pl-2"}>
+                {data.title}
+              </span>
+            </div>
+            <div className="items-center">
+              <button
+                className="px-4 py-2 float-right"
+                onClick={() => handleClick(data.id)}
+              >
+                x
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
